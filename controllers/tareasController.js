49c9e78f1c456f455f78b0cd0167bfd2ceec8cc6
Gear_Felipe_117 <julipe.andres@gmail.com>
2019-10-02 13:41:44 -0500

    ultimo commit de clase, faltan algunos metodos por realizar

--- controllers/tareasController.js
+++ controllers/tareasController.js
@@ -6,10 +6,13 @@ class Tareas{
         this.listaTareas.push(tarea)
     }
     modificarTarea(id, tarea){
-
+        let index = this.listaTareas.findIndex(element => element.id == id)
+        this.listaTareas.splice(index, 1, tarea)
     }
     eliminarTarea(id){
-
+        let index = this.listaTareas.findIndex(element => element.id == id)
+        this.listaTareas.splice(index, 1)
+        console.log(this.listaTareas);
     }
     obtenerTareas(){
         return this.listaTareas
--- routes/tareasRouter.js
+++ routes/tareasRouter.js
@@ -23,4 +23,25 @@ router.post('/tareas/', (req,res)=>{
     res.send(respuesta)
 })
 
+router.delete("/tareas/:id",(req,res)=>{
+    let id = req.params.id
+    misTareas.eliminarTarea(id)
+    let respuesta = {}
+    respuesta.estado = true
+    respuesta.informacion = {}
+    respuesta.mensaje = "Tarea eliminada con éxito"
+    res.send(respuesta)
+})
+
+router.put("/tareas/:id", (req,res)=>{
+    let id = req.params.id
+    let tarea = req.body
+    misTareas.modificarTarea(id, tarea)
+    let respuesta = {}
+    respuesta.estado = true
+    respuesta.informacion = {}
+    respuesta.mensaje = "Tarea modificada con éxito"
+    res.send(respuesta)
+})
+
 module.exports = router
