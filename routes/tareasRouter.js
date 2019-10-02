605dfec777c426a217b120c602ce931170e53887
Gear_Felipe_117 <julipe.andres@gmail.com>
2019-10-02 13:24:18 -0500

    segundo commit de intento cambiar credenciales

--- routes/tareasRouter.js
+++ routes/tareasRouter.js
@@ -21,7 +21,6 @@ router.post('/tareas/', (req,res)=>{
     respuesta.informacion = true
     respuesta.mensaje = "Tarea agregada con éxito"
     res.send(respuesta)
-    
 })
 
 module.exports = router
